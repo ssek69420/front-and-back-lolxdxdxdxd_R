abb6f02855ae18800451a3943bee8044d9b29091
ssek69420 <kaikevix2233@gmail.com>
2025-10-01 14:34:25 -0300

    added more tests to userController because teach said to

--- backend/__tests__/userController.unit.test.js
+++ backend/__tests__/userController.unit.test.js
@@ -20,8 +20,48 @@ what a waste of time
 change of plans, im not
 deal with it
 */
-const controller = require('../index.js');
+const userController = require('../controllers/userController.js');
+const userRoutes = require('../routes/userRoutes.js');
+const index = require('../index.js');
 
 test('userController is defined', () => {
-  expect(controller).toBeDefined();
+  expect(userController).toBeDefined();
+});
+//userController to not be defined, will come in handy when
+//I'm trying to break the app
+test('userController is not defined', () => {
+  expect(userController).not.toBeDefined();
+})
+
+test('a whole lotta functions to be expected as functions', () => {
+  expect(typeof userController.inscribe).toBe('function');
+  expect(typeof userController.login).toBe('function');
+  expect(typeof userController.verifyAuth).toBe('function');
+  expect(typeof userController.verifyIsAdmin).toBe('function');
+  //start of the wrong part of the test
+  
+  //num
+  expect(typeof userController.inscribe).toBe('number');
+  expect(typeof userController.login).toBe('number');
+  expect(typeof userController.verifyAuth).toBe('number');
+  expect(typeof userController.verifyIsAdmin).toBe('number');
+  //str
+  expect(typeof userController.inscribe).toBe('string');
+  expect(typeof userController.login).toBe('string');
+  expect(typeof userController.verifyAuth).toBe('string');
+  expect(typeof userController.verifyIsAdmin).toBe('string');
+});
+
+//simpple route test
+test('theres /inscribe and /login', () => {
+  const paths = userRoutes.stack
+  .map(layer => layer.route?.path)
+  .filter(Boolean);
+  expect(paths).toContain('/inscribe');
+  expect(paths).toContain('/login');
+});
+
+
+test('index loads :O', () => {
+  expect(index).toBeDefined();
 });
